75e6e2e899a18bc784485d2ba73cbad64b73eed5
freelancersazal <freelancersazal@gmail.com>
2024-06-18 19:27:21 +0600

    Home 3 updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,7 +183,43 @@ $(function () {
     });
 
 
+    // PORTFOLIO 3 SLIDER
+    $('.portfolio_3_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: false,
 
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
 
 
 
